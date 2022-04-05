bd214fcd8539d466312e7104cc8335b31cbd4a98
Bruno <bruno.dearaujo@polito.it>
2022-04-05 14:55:53 +0200

    Example change

--- examples/basic_plots.py
+++ examples/basic_plots.py
@@ -5,6 +5,8 @@ pop = [2.5, 3.5, 4.6, 4.9, 5.3, 5.9, 6.9, 7.8, 8.4, 9.2]
 
 values = [0.01, 0.5, 3.7, 5.0, 4.2, 2.7, 4.3, 8.3]
 
+value = values.extend(2)
+
 plt.plot(year, pop)                     # Curve
 # plt.scatter(year, pop)                # Points
 # plt.hist(values, bins=3)              # Histogram
